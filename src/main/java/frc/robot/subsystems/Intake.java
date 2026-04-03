78f2126d38f97c34fe9fc3fdb6b90fd187ed37c5
FRC-PlatyPirates-9181 <122500810+FRC-PlatyPirates-9181@users.noreply.github.com>
2026-04-03 10:56:34 -0500

    had to change something really quickly sad it happened on main swiching to hotfixes2 now

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -20,7 +20,7 @@ public class Intake extends SubsystemBase {
 
     // constructor activates once when the robot turns on
     public Intake() {
-        intakeMotor = new SparkFlex(Constants.DriveConstants.kHoodMotorCanId, SparkLowLevel.MotorType.kBrushless);
+        intakeMotor = new SparkFlex(Constants.DriveConstants.kIntakeArmMotorCanId, SparkLowLevel.MotorType.kBrushless);
         scooperMotor = new SparkMax(Constants.DriveConstants.kIntakeScooperMotorCanId, MotorType.kBrushless);
     }
     
